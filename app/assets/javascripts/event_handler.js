e767351ba58ae20fdee3e16669131da8af23638f
Kenneth To <kennethkto@gmail.com>
2015-10-26 16:40:38 -0700

    add event handler file and page swapping css class selectors to part 2

---
+++ app/assets/javascripts/event_handler.js
@@ -0,0 +1,18 @@
+$(function(){
+
+  // part 2 event handlers
+  // default sections hidden
+  $(".part2-page2").hide();
+  // Buttons - Hiding and Showing
+  function buttonSelect(button, select, unselect1) {
+    $(button).on('click', function(){
+      $(select).show();
+      $(unselect1).hide();
+    });
+  }
+  // Instantiating buttons
+  buttonSelect(".pt2-pg1-btn", ".part2-page2", ".part2-page1");
+  buttonSelect(".pt2-pg2-btn", ".part2-page1", ".part2-page2");
+
+
+});
