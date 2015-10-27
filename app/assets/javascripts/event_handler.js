c9aed43dbc71d0ff40fab6d6405bf4d519146935
Kenneth To <kennethkto@gmail.com>
2015-10-27 11:49:37 -0700

    add default hidden table in part 2 page 2 and show functionality with use button

--- app/assets/javascripts/event_handler.js
+++ app/assets/javascripts/event_handler.js
@@ -3,6 +3,7 @@ $(function(){
   // part 2 event handlers
   // default sections hidden
   $(".part2-page2").hide();
+  $(".whatwedotable").hide();
   // Buttons - Hiding and Showing
   function buttonSelect(button, select, unselect1) {
     $(button).on('click', function(){
@@ -13,6 +14,9 @@ $(function(){
   // Instantiating buttons
   buttonSelect(".pt2-pg1-btn", ".part2-page2", ".part2-page1");
   buttonSelect(".pt2-pg2-btn", ".part2-page1", ".part2-page2");
+
+  // part 2 what we do table reveal
+  buttonSelect(".whatwedo-btn", ".whatwedotable");
 
 
 });
