1a980f5d1b86b056bf0e645085bbe13968584931
Kenneth To <kennethkto@gmail.com>
2015-12-01 10:37:54 -0800

    remove right arrows for section browsing

--- app/assets/javascripts/event_handler.js
+++ app/assets/javascripts/event_handler.js
@@ -1,22 +1,22 @@
-$(function(){
-
-  // part 2 event handlers
-  // default sections hidden
-  $(".part2-page2").hide();
-  $(".whatwedotable").hide();
-  // Buttons - Hiding and Showing
-  function buttonSelect(button, select, unselect1) {
-    $(button).on('click', function(){
-      $(select).show();
-      $(unselect1).hide();
-    });
-  }
-  // Instantiating buttons
-  buttonSelect(".pt2-pg1-btn", ".part2-page2", ".part2-page1");
-  buttonSelect(".pt2-pg2-btn", ".part2-page1", ".part2-page2");
-
-  // part 2 what we do table reveal
-  buttonSelect(".whatwedo-btn", ".whatwedotable");
-
-
-});
+// $(function(){
+//
+//   // part 2 event handlers
+//   // default sections hidden
+//   $(".part2-page2").hide();
+//   $(".whatwedotable").hide();
+//   // Buttons - Hiding and Showing
+//   function buttonSelect(button, select, unselect1) {
+//     $(button).on('click', function(){
+//       $(select).show();
+//       $(unselect1).hide();
+//     });
+//   }
+//   // Instantiating buttons
+//   buttonSelect(".pt2-pg1-btn", ".part2-page2", ".part2-page1");
+//   buttonSelect(".pt2-pg2-btn", ".part2-page1", ".part2-page2");
+//
+//   // part 2 what we do table reveal
+//   buttonSelect(".whatwedo-btn", ".whatwedotable");
+//
+//
+// });
